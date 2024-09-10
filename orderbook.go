3332e5526354b105c770cc396e02762582ca551f
potzrebie <johan.bjurgert@gmail.com>
2024-09-10 12:34:34 +0200

    Add functions and fields to OrderBook

--- orderbook.go
+++ orderbook.go
@@ -12,16 +12,41 @@ type Order struct {
 	Next   *Order
 }
 
+// PriceLevel keeps track of not yet executed orders for a given price level.
 type PriceLevel struct {
 	Head *Order
 	Tail *Order
 }
 
+// OrderBook stores all orders for all price levels of a given book. It also keeps track of best bid and offer.
 type OrderBook struct {
+	BestBid     int
+	BestOffer   int
 	PriceLevels []*PriceLevel
 }
 
 func NewOrderBook(size int) *OrderBook {
-	return &OrderBook{PriceLevels: make([]*PriceLevel, size)}
+	return &OrderBook{BestBid: 0,
+		BestOffer:   MAX_PRICE,
+		PriceLevels: make([]*PriceLevel, size)}
+
+}
+
+func (ob *OrderBook) AddOrder(o *Order) {
+	ob.PriceLevels[o.Price].Tail.Next = o
+	ob.PriceLevels[o.Price].Tail = o
+}
+
+func getLevelVolume(pl *PriceLevel) int {
+	sum := 0
+	currentOrder := pl.Head
+
+	// Traverse the linked list
+	for currentOrder != nil {
+		sum += currentOrder.Volume
+		currentOrder = currentOrder.Next
+	}
+
+	return sum
 
 }
