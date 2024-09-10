4920b8a9e6a4465e7e74f0dc1a683efe5064b7c2
potzrebie <johan.bjurgert@gmail.com>
2024-09-10 14:25:13 +0200

    Add test TestOrderBookVolumeAggregateCorrect)

--- exchange_test.go
+++ exchange_test.go
@@ -4,11 +4,36 @@ import (
 	"testing"
 )
 
-func TestNewOrderBook(t *testing.T) {
+func TestNewOrderBookSizeCorrect(t *testing.T) {
 	desired_size := 10000
 	ob := NewOrderBook(desired_size)
 	if len(ob.PriceLevels) != desired_size {
 		t.Errorf("Expected size %v but got %v ", desired_size, len(ob.PriceLevels))
 	}
 
+}
+
+func TestOrderBookVolumeAggregateCorrect(t *testing.T) {
+	o1 := Order{
+		Id:     1,
+		IsBuy:  true,
+		Price:  3,
+		Volume: 1,
+		Next:   nil,
+	}
+	o2 := Order{
+		Id:     2,
+		IsBuy:  true,
+		Price:  3,
+		Volume: 1,
+		Next:   nil,
+	}
+	ob := NewOrderBook(4)
+	ob.AddOrder(&o1)
+	ob.AddOrder(&o2)
+	pl := ob.PriceLevels[3]
+	qty := getLevelVolume(pl)
+	if qty != 2 {
+		t.Errorf("Expected volume %v but got %v ", 2, qty)
+	}
 }
