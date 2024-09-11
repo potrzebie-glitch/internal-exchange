18742127a2fc4dd9a8952af576cdad682d104bc9
potzrebie <johan.bjurgert@gmail.com>
2024-09-11 22:56:59 +0200

    Report trades through channel

--- main.go
+++ main.go
@@ -10,20 +10,25 @@ import (
 const ITERATIONS = 10000000
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	orderBook := exchange.NewOrderBook(exchange.MAX_PRICE)
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	orderBook := exchange.NewOrderBook()
 	engine := exchange.NewMatchingEngine(orderBook)
+	engine.StartTradeProcessor()
 	start := time.Now()
 	for i := 0; i < ITERATIONS; i++ {
-		isBuy := rand.Intn(2) == 1
-		price := rand.Intn(21) + 90
+		isBuy := r.Intn(2) == 1
+		price := r.Intn(21) + 90
+		volume := r.Intn(5) + 1
 		order := &exchange.Order{
 			Id:     i,
 			IsBuy:  isBuy,
 			Price:  price,
-			Volume: 10,
+			Volume: volume,
 		}
+		//fmt.Printf("Market is %v @ %v", engine.OrderBook.BestBid, engine.OrderBook.BestOffer)
 		engine.ProcessOrder(order)
+
 	}
 	duration := time.Since(start)
 	fmt.Printf("Processing %v orders takes %v ", ITERATIONS, duration)
