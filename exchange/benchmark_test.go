5ebc83f183de06f8a856d3c1e4051b89473a86ab
potzrebie <johan.bjurgert@gmail.com>
2024-09-11 14:06:11 +0200

    Add benchmark test

---
+++ exchange/benchmark_test.go
@@ -0,0 +1,37 @@
+// matchingengine_test.go
+package exchange
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func BenchmarkProcessOrder(b *testing.B) {
+	// Seed the random number generator for randomness
+	rand.Seed(time.Now().UnixNano())
+
+	// Create a sample OrderBook and MatchingEngine
+	orderBook := NewOrderBook(1000)
+	engine := NewMatchingEngine(orderBook)
+
+	// Benchmarking loop - runs b.N times
+	for i := 0; i < b.N; i++ {
+		// Randomize whether it's a buy or sell order (true = buy, false = sell)
+		isBuy := rand.Intn(2) == 1
+
+		// Randomize the price between 90 and 110
+		price := rand.Intn(21) + 90 // Generates a price between 90 and 110
+
+		// Create a randomized order
+		order := &Order{
+			Id:     i, // Use loop index as the order ID
+			IsBuy:  isBuy,
+			Price:  price,
+			Volume: 10,
+		}
+
+		// Process the randomized order
+		engine.ProcessOrder(order)
+	}
+}
